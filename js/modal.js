294bea7ce1bb075aa2786048dc3674e105a6e618
ievsepl <107007762+ievsepl@users.noreply.github.com>
2022-07-26 14:49:23 +0300

    add all hw-8

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,26 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('is-hidden');
   }
+})();
+(() => {
+  const openMenuBtn = document.querySelector('[data-menu-open]');
+  const closeMenuBtn = document.querySelector('[data-menu-close]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  openMenuBtn.addEventListener('click', () => {
+    // const expanded = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+
+    // openMenuBtn.classList.toggle('is-open');
+    // openMenuBtn.setAttribute('aria-expanded', expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+  });
+  closeMenuBtn.addEventListener('click', () => {
+    // closeMenuBtn.classList.toggle('is-open');
+
+    mobileMenuRef.classList.toggle('is-open');
+  });
 })();
